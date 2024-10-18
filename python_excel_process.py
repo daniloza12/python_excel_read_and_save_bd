5dd176a6c9b6ead3e93faa01dceb83e1dcd2ea5c
DaniLo-Za <87085703+daniloza12@users.noreply.github.com>
2024-10-17 21:59:57 -0500

    Update python_excel_process.py

--- python_excel_process.py
+++ python_excel_process.py
@@ -22,23 +22,21 @@ DATABASE_TYPE = 'mysql'
 DBAPI = 'pymysql'
 USER = 'root2'
 PASSWORD = '123456'
-HOST = 'localhost'
-# HOST = 'host.docker.internal'
+# HOST = 'localhost'
+HOST = 'host.docker.internal'
 PORT = '3306'
 DATABASE = 'db_pam_cliente'
 DB_TABLE = 'tbl_cliente'
 
 # Crear la cadena de conexión
-print("conectando inicio")
 logger.info("Conectando a BD")
 connection_string = f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
 engine = create_engine(connection_string)
-print("conectando inicio")
 
 logger.info("Conexion a BD success")
 
-excel_file  = '.\\resources\\clientes_excel_2.xlsx'
-#excel_file = './resources/clientes_excel_2.xlsx'
+#excel_file  = '.\\resources\\clientes_excel_2.xlsx'
+excel_file = './resources/clientes_excel_2.xlsx'
 
 # columnas de la tabla de la bd
 # dni	cliente_id	telefono	correo	estado	password	rol	apellidos	nombre	direccion
@@ -52,15 +50,14 @@ def calcular_diferencia(inicio, fin):
     diferencia = fin - inicio
     horas, resto = divmod(diferencia.total_seconds(), 3600)
     minutos, segundos = divmod(resto, 60)
-    print(f"Diferencia: {int(horas)} horas, {int(minutos)} minutos, {int(segundos)} segundos")
- 
- 
+    logger.info(f"Diferencia: {int(horas)} horas, {int(minutos)} minutos, {int(segundos)} segundos")
+  
 try:
     hora_inicial = obtener_hora_actual()
 
     # Lectura del excel
     data = pd.read_excel(excel_file, engine='openpyxl')
-    print(f"Archivo cargado con éxito. Total de registros: {len(data)}")
+    logger.info(f"Archivo cargado con éxito. Total de registros: {len(data)}")
 
     # Tamaño de fragmentos para cada inserción
     chunk_size = 5
@@ -71,16 +68,21 @@ try:
         
         # Guarda el fragmento en la base de datos
         chunk.to_sql(DB_TABLE, con=engine, if_exists='append', index=False)
-        print(f'{len(chunk)} Registros insertados correctamente.')
+        logger.info(f'{len(chunk)} Registros insertados correctamente.')
     
-    print("Datos guardados exitosamente.")
+    logger.info("Datos guardados exitosamente.")
     
     # Calcular tiempo de ejecución
     hora_final = obtener_hora_actual()    
     calcular_diferencia(hora_inicial, hora_final)
 
-    print("Proceso completado exitosamente.")
+    logger.info("Proceso completado exitosamente.")
     
 except Exception as e:
-    print(f"Error al procesar el archivo o insertar datos: {e}")
+    logger.info(f"Error al procesar el archivo o insertar datos: {e}")
+
+
+
+
+
 
